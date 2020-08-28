ba2c504339eb77f133c1330fd6f84cb41460de21
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-08-28 13:18:55 +0530

    resolved regex bug for validation of bas16

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NumberSystem from "./Components/NumberSystem";
 const inputValidation = {
   2: new RegExp("^[0-1]*$"),
   8: new RegExp("^[0-7]*$"),
-  16: new RegExp("^[0-15]*$"),
+  16: new RegExp("^[0-9]*$"),
 };
 
 function App() {
